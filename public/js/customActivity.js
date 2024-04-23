094040107e72449d9e2986218e49040978959556
anderson-fullsix <144189416+anderson-fullsix@users.noreply.github.com>
2024-04-23 18:52:06 -0300

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -130,7 +130,7 @@ function(eventDefinitionModel) {
 payload['arguments'].execute.inArguments = [{
     "Mobile": "{{Contact.Attribute." + eventDefinitionKey + ".Mobile}}"
 }];
-        console.log("payload: " + payload.inArguments[0].Mobile);
+        console.log("payload: " + payload.execute.inArguments[0].Mobile);
         
         payload['metaData'].isConfigured = true;
 
