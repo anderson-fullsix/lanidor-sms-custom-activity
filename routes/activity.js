5d2b8fd5162f1ebb0bd728047b08ef947bf1019c
anderson-fullsix <144189416+anderson-fullsix@users.noreply.github.com>
2024-08-13 20:02:42 -0300

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -134,10 +134,10 @@ decoded.inArguments[0].recipients.forEach(obj => {
                                 phoneNumbers.push(obj[key]);
                             }
                             recipients.push({ "Mobile": phoneNumbers.join(', ') });
-console.log("let recipients: ", recipients);
-console.log("decoded.inArguments[0]: ", decoded.inArguments[0]);
-console.log("decoded.inArguments: ", decoded.inArguments)
                         }
+    console.log("let recipients: ", recipients);
+console.log("decoded.inArguments[0]: ", decoded.inArguments[0]);
+console.log("decoded.inArguments: ", decoded.inArguments);
                     });
     
 let data = JSON.stringify({
