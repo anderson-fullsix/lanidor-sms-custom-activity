c7fb99bbb2979e3b602c991a7bc00f6128e913ae
anderson-fullsix <144189416+anderson-fullsix@users.noreply.github.com>
2024-09-11 06:30:01 -0300

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -50,7 +50,7 @@ function logData(req) {
     console.log("originalUrl: " + req.originalUrl);
 }
 
-
+/*
 // Configurações da API do Marketing Cloud
 const options = {
     auth: {
@@ -94,7 +94,7 @@ async function saveToDataExtension(data) {
         console.error('Erro ao gravar na Data Extension:', err);
     }
 }
-
+*/
 
 /*
  * POST Handler for / route of Activity (this is the edit route).
@@ -265,7 +265,7 @@ axios.request(config_post)
 .then((response) => {
   console.log("JSON stringify: ", JSON.stringify(response.data));
   // Chama a função para gravar os dados na Data Extension após a resposta
-  saveToDataExtension(response.data);
+  //saveToDataExtension(response.data);
 })
 .catch((error) => {
   console.log("Error axios.request response: ", error);
