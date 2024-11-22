348a600099be6ceeb17a0114fd7c9e3c16509323
anderson-fullsix <144189416+anderson-fullsix@users.noreply.github.com>
2024-11-21 22:46:51 -0300

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -107,38 +107,7 @@ define([
         authTokens = tokens;
 
 	connection.on('requestedTokens', function(tokens) {
-	    //Code to request tokens
-	    /*
-	    if (!authToken) {
-	    	const authData = {
-		    username: 'AAA',
-		    password: 'BBB'
-    	    	};
-
-	        // Make a request to your authentication endpoint to get the token
-	        const authURL = 'https://www.abinfo.pt/api/sms/auth/login'
-  	        fetch(authURL, {
-		    method: 'GET',
-		    headers: {
-			'Authorization': 'Basic ${btoa('${authData.username}:${authData.password}')}'
-		    }
-	    	})
-	    	.then(response => response.json())
-		.then(data => {
-	            // Use the token for subsequent requests
-		    console.log('*** ongettokens ***');
-		    authToken = data.token;
-		    tokens.resolve(authToken);
-	        })
-	    	.catch(error => {
-		    console.error('Error:', error);
-		    tokens.reject(error);
-	    	});
-            } else {
-		// Token already obtained, resolve immediately
-		tokens.resolve(authToken);
-	    }
-	*/
+	    
 	});
 
 	connection.on('execute', function(events) {
