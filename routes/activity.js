be8af28ff3233dafddb442b4305ed80899544f92
anderson-fullsix <144189416+anderson-fullsix@users.noreply.github.com>
2024-11-21 22:13:34 -0300

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -1,13 +1,11 @@
 'use strict';
 var util = require('util');
 
-// Deps
 const Path = require('path');
 const JWT = require(Path.join(__dirname, '..', 'lib', 'jwtDecoder.js'));
 var util = require('util');
 var http = require('https');
 const axios = require('axios');
-//const FuelRest = require('fuel-rest');
 
 exports.logExecuteData = [];
 
@@ -31,321 +29,135 @@ function logData(req) {
         secure: req.secure,
         originalUrl: req.originalUrl
     });
-/*
-    console.log("body: " + util.inspect(req.body));
-    console.log("headers: " + req.headers);
-    console.log("trailers: " + req.trailers);
-    console.log("method: " + req.method);
-    console.log("url: " + req.url);
-    console.log("params: " + util.inspect(req.params));
-    console.log("query: " + util.inspect(req.query));
-    console.log("route: " + req.route);
-    console.log("cookies: " + req.cookies);
-    console.log("ip: " + req.ip);
-    console.log("path: " + req.path);
-    console.log("host: " + req.host);
-    console.log("fresh: " + req.fresh);
-    console.log("stale: " + req.stale);
-    console.log("protocol: " + req.protocol);
-    console.log("secure: " + req.secure);
-    console.log("originalUrl: " + req.originalUrl);
-*/
 }
 
-/*
-// Função para obter o token OAuth
-async function getOAuthToken() {
-    const authResponse = await axios.post('https://auth.exacttargetapis.com/v1/requestToken', {
-        clientId: '0zi6qnu4vepdfsotelvxeimj',
-        clientSecret: 'KRjVa8UgYVqRDlbmHIipkpiW'
-    });
-    return authResponse.data.accessToken;
-}
-
-// Função para gravar os dados na Data Extension
-async function saveToDataExtension(data) {
-    const dePayload = {
-        "items": [
-            {
-                "id": data.id,
-                "partnerId": data.partnerId,
-                "sender": data.sender,
-                "description": data.description,
-                "text": data.text,
-                "user": data.user,
-                "registrationDate": data.registrationDate,
-                "records": data.records,
-                "status": data.status,
-                "sentDate": data.sentDate,
-                "error": data.error
-            }
-        ]
-    };
-
-    try {
-        // Obter o token OAuth antes de fazer a requisição
-        const accessToken = await getOAuthToken();
-
-        const response = await axios.post(
-            'https://YOUR_SUBDOMAIN.rest.marketingcloudapis.com/hub/v1/dataevents/key:65659DF6-6DED-4C78-9CF3-5683939E4F36/rowset',
-            dePayload,
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${accessToken}`
-                }
-            }
-        );
-
-        console.log('Dados gravados na Data Extension:', response.data);
-    } catch (err) {
-        console.error('Erro ao gravar na Data Extension:', err);
-    }
-}
-*/
-
-/*
- * POST Handler for / route of Activity (this is the edit route).
- */
 exports.edit = function (req, res) {
-    // Data from the req and put it in an array accessible to the main app.
-    //console.log( req.body );
     logData(req);
     res.send(200, 'Edit');
 };
 
-/*
- * POST Handler for /save/ route of Activity.
- */
 exports.save = function (req, res) {
-    // Data from the req and put it in an array accessible to the main app.
-    //console.log( req.body );
     logData(req);
     res.send(200, 'Save');
 };
 
-/*
- * POST Handler for /execute/ route of Activity.
- */
 exports.execute = function (req, res) {
 require('dotenv').config();
-    // example on how to decode JWT
     JWT(req.body, process.env.jwtSecret, (err, decoded) => {
 
-        // verification error -> unauthorized request
         if (err) {
             console.error(err);
             return res.status(401).end();
         }
 
         if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
-/*
-  const requestOptions = {
-  method: "POST",
-  body: "teste",
-  redirect: "follow"
-};
-
-axios.get('https://enickt5gs0alo.x.pipedream.net')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
- */           
-
-/*            
-var token = null
-let config_get = {
-  method: 'get',
-  maxBodyLength: Infinity,
-  url: 'https://www.abinfo.pt/api/sms/auth/login',
-  headers: { 
-    'Authorization': 'Basic YW5kZXJzb24ubWVuZGVzLWV4dEBmdWxsc2l4LnB0Okxhbmlkb3IjMjAyNCE='
-  },
-  data : ""
-};
-*/
-
-var token = null
-const authToken = process.env.AUTH_TOKEN;
-const urlLogin = process.env.URL_LOGIN;
-
-const config_get = {
-  method: 'get',
-  maxBodyLength: Infinity,
-  url: urlLogin,
-  headers: {
-    'Authorization': `Basic ${authToken}`,
-  },
-  data: '',
-};
-
-axios.request(config_get)
-.then((response) => {
-/*
-  console.log(JSON.stringify(response.data));
-*/
-  token = response.data.token
-/*
-  console.log("token interno: ", token)
-*/
-
-/*
-let recipients = decoded.inArguments[0].recipients.map(recipient => {
-  return { "Mobile": recipient.Mobile };
-});
-*/
-    
-let recipients = [];
-let mobileNumber = '';
-let variavel = '';
-let variavel2 = '';
-let variavel3 = '';
-let messageText = ''; // Variável para armazenar o texto da mensagem
-
-/*
-console.log("decoded.inArguments: ", JSON.stringify(decoded.inArguments));
-console.log("decoded.inArguments[0]: ", decoded.inArguments[0]);
-*/
-
-// Itera sobre os inArguments
-decoded.inArguments.forEach(arg => {
-    // Verifica se variavel está presente
-    if (arg.variavel) {
-        variavel = arg.variavel;
-/*        console.log("variavel: ", variavel);
-    } else {
-        console.log("variavel não encontrado em: ", arg); */ 
-    }
-
-    // Verifica se variavel2 está presente
-    if (arg.variavel2) {
-        variavel2 = arg.variavel2;
-/*        console.log("variavel2: ", variavel2);
-    } else {
-        console.log("variavel2 não encontrado em: ", arg); */
-    }
-
-    // Verifica se variavel3 está presente
-    if (arg.variavel3) {
-        variavel3 = arg.variavel3;
-/*
-        console.log("variavel3: ", variavel3);
-    } else {
-        console.log("variavel3 não encontrado em: ", arg); */
-    }
-    
-    // Verifica se o texto da mensagem está presente
-    if (arg.text) {
-        messageText = arg.text;
-/*        console.log("Texto da mensagem: ", messageText); */
-    }
-
-    // Itera sobre os destinatários para buscar o número de telefone
-    if (arg.recipients) {
-        arg.recipients.forEach(obj => {
-            for (let key in obj) {
-                if (obj.hasOwnProperty(key) && key.startsWith('Mobile')) {
-                    recipients.push({ "Mobile": obj[key] });
-                    mobileNumber = obj[key];
-/*                    console.log("recipients: ", recipients); */
-                }
-            }
-        });
-    }
-});
-
-if (variavel) {
-    messageText = messageText.replace("<<variavel>>", variavel);
-/*    console.log("Mensagem final: ", messageText);
-} else {
-    console.log("variavel está indefinido. Substituição não realizada."); */
-}
-
-if (variavel2) {
-    messageText = messageText.replace("<<variavel2>>", variavel2);
-/*    console.log("Mensagem final: ", messageText);
-} else {
-    console.log("variavel2 está indefinido. Substituição não realizada."); */
-}
-
-if (variavel3) {
-    messageText = messageText.replace("<<variavel3>>", variavel3);
-/*    console.log("Mensagem final: ", messageText);
-} else {
-    console.log("variavel3 está indefinido. Substituição não realizada."); */
-}
-    
-// Gerando um messageId único
-const uniqueMessageId = generateUniqueMessageId(mobileNumber);
-/* console.log("Unique Message ID: ", uniqueMessageId); */
-
-let data = JSON.stringify({
-  "id": uniqueMessageId,
-  "description": decoded.inArguments[0].description,
-  "sender": decoded.inArguments[0].sender,
-  "partnerId": decoded.inArguments[0].partnerId,
-//  "text": decoded.inArguments[0].text,
-  "text": messageText,
-  "variavel": decoded.inArguments[0].variavel,
-  "variavel2": decoded.inArguments[0].variavel2,
-  "variavel3": decoded.inArguments[0].variavel3,
-  "sendnow": "true",
-  "recipients": recipients
-});
-
-/*
-console.log("**** payload sent to Client server ****");
-console.log("id: ", uniqueMessageId);
-console.log("description: ", decoded.inArguments[0].description);
-console.log("sender: ", decoded.inArguments[0].sender);
-console.log("partnerId: ", decoded.inArguments[0].partnerId);
-//console.log("text: ", decoded.inArguments[0].text);
-console.log("text: ", messageText);
-console.log("variavel: ", decoded.inArguments[0].variavel);
-console.log("variavel2: ", decoded.inArguments[0].variavel2);
-console.log("variavel3: ", decoded.inArguments[0].variavel3);
-console.log("sendnow: ", "true");
-console.log("Mobile: ", decoded.inArguments[0].recipients[0].Mobile);
-console.log("recipients: ", recipients);
-*/
-
-const urlAction = process.env.URL_ACTION;
-    
-let config_post = {
-  method: 'post',
-  maxBodyLength: Infinity,
-  url: urlAction,
-  headers: { 
-    'Content-Type': 'application/json', 
-    'Authorization': 'Bearer ' + token
-  },
-  data : data
-};
-
-axios.request(config_post)
-.then((response) => {
-  /*console.log("JSON stringify: ", JSON.stringify(response.data)); */
-  // Chama a função para gravar os dados na Data Extension após a resposta
-  //saveToDataExtension(response.data);
-})
-.catch((error) => {
-  console.log("Error axios.request response: ", error);
-});
-})
-.catch((error) => {
-  console.log("Error axios.request: ", error);
-});
-
-/* console.log("token externo: ", token) */
+            var token = null
+            const authToken = process.env.AUTH_TOKEN;
+            const urlLogin = process.env.URL_LOGIN;
             
-            // decoded in arguments
+            const config_get = {
+              method: 'get',
+              maxBodyLength: Infinity,
+              url: urlLogin,
+              headers: {
+                'Authorization': `Basic ${authToken}`,
+              },
+              data: '',
+            };
+
+            axios.request(config_get)
+            .then((response) => {
+                token = response.data.token
+
+                let recipients = [];
+                let mobileNumber = '';
+                let variavel = '';
+                let variavel2 = '';
+                let variavel3 = '';
+                let messageText = '';
+
+                decoded.inArguments.forEach(arg => {
+                
+                    if (arg.variavel) {
+                        variavel = arg.variavel;
+                    }
+                
+                    if (arg.variavel2) {
+                        variavel2 = arg.variavel2;
+                    }
+                
+                    if (arg.variavel3) {
+                        variavel3 = arg.variavel3;
+                    }
+                    
+                    if (arg.text) {
+                        messageText = arg.text;
+                    }
+    
+                    if (arg.recipients) {
+                        arg.recipients.forEach(obj => {
+                            for (let key in obj) {
+                                if (obj.hasOwnProperty(key) && key.startsWith('Mobile')) {
+                                    recipients.push({ "Mobile": obj[key] });
+                                    mobileNumber = obj[key];
+                                }
+                            }
+                        });
+                    }
+                });
+
+                if (variavel) {
+                    messageText = messageText.replace("<<variavel>>", variavel);
+                }
+                
+                if (variavel2) {
+                    messageText = messageText.replace("<<variavel2>>", variavel2);
+                }
+                
+                if (variavel3) {
+                    messageText = messageText.replace("<<variavel3>>", variavel3);
+                }
+        
+                const uniqueMessageId = generateUniqueMessageId(mobileNumber);
+                
+                let data = JSON.stringify({
+                  "id": uniqueMessageId,
+                  "description": decoded.inArguments[0].description,
+                  "sender": decoded.inArguments[0].sender,
+                  "partnerId": decoded.inArguments[0].partnerId,
+                  "text": messageText,
+                  "variavel": decoded.inArguments[0].variavel,
+                  "variavel2": decoded.inArguments[0].variavel2,
+                  "variavel3": decoded.inArguments[0].variavel3,
+                  "sendnow": "true",
+                  "recipients": recipients
+                });
+    
+                const urlAction = process.env.URL_ACTION;
+                    
+                let config_post = {
+                  method: 'post',
+                  maxBodyLength: Infinity,
+                  url: urlAction,
+                  headers: { 
+                    'Content-Type': 'application/json', 
+                    'Authorization': 'Bearer ' + token
+                  },
+                  data : data
+                };
+
+                axios.request(config_post)
+                .then((response) => {
+                })
+                .catch((error) => {
+                    console.log("Error axios.request response: ", error);
+                });
+            })
+            .catch((error) => {
+              console.log("Error axios.request: ", error);
+            });
+
             var decodedArgs = decoded.inArguments[0];
-/*            console.log("decoded.inArguments[0]: ", decoded.inArguments[0]);       */
             logData(req);
             res.send(200, 'Execute');
         } else {
@@ -355,28 +167,17 @@ axios.request(config_post)
     });
 };
 
-
-/*
- * POST Handler for /publish/ route of Activity.
- */
 exports.publish = function (req, res) {
-    // Data from the req and put it in an array accessible to the main app.
-    //console.log( req.body );
     logData(req);
     res.send(200, 'Publish');
 };
 
-/*
- * POST Handler for /validate/ route of Activity.
- */
 exports.validate = function (req, res) {
-    // Data from the req and put it in an array accessible to the main app.
-    //console.log( req.body );
     logData(req);
     res.send(200, 'Validate');
 };
 
 function generateUniqueMessageId(mobileNumber) {
-    const timestamp = new Date().getTime();  // Usa apenas o timestamp
-    return `MSG-${mobileNumber}-${timestamp}`;  // Retorna o ID único baseado no timestamp
+    const timestamp = new Date().getTime(); 
+    return `MSG-${mobileNumber}-${timestamp}`;
 }
