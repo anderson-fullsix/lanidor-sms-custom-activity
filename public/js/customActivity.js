f44e9d3f975d7d6411c09688c882e74d5c207c32
anderson-fullsix <144189416+anderson-fullsix@users.noreply.github.com>
2024-11-21 22:15:27 -0300

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -107,65 +107,7 @@ define([
         authTokens = tokens;
 
 	connection.on('requestedTokens', function(tokens) {
-	    //Code to request tokens
-	    /*
-	    if (!authToken) {
-	    	const authData = {
-		    username: 'AAA',
-		    password: 'BBB'
-    	    	};
-
-	        // Make a request to your authentication endpoint to get the token
-	        const authURL = 'https://www.abinfo.pt/api/sms/auth/login'
-  	        fetch(authURL, {
-		    method: 'GET',
-		    headers: {
-			'Authorization': 'Basic ${btoa('${authData.username}:${authData.password}')}'
-		    }
-	    	})
-	    	.then(response => response.json())
-		.then(data => {
-	            // Use the token for subsequent requests
-		    console.log('*** ongettokens ***');
-		    authToken = data.token;
-		    tokens.resolve(authToken);
-	        })
-	    	.catch(error => {
-		    console.error('Error:', error);
-		    tokens.reject(error);
-	    	});
-            } else {
-		// Token already obtained, resolve immediately
-		tokens.resolve(authToken);
-	    }
-	*/
 	});
-
-	connection.on('execute', function(events) {
-	    // Execute the activity
-  	    const token = events.token;
-
-	    // Use the token obtained earlier
-	    const commURL = 'https://enickt5gs0alo.x.pipedream.net'
-  	    fetch(commURL, {
-    	    	method: 'POST',
-    	    	body: JSON.stringify(payload),
-    	    	headers: {
-      		    'Authorization': `Bearer ${token}`,
-      		    'Content-Type': 'application/json'
-    	    	}
-  	    })
-  	    .then(response => response.json())
-  	    .then(data => {
-		console.log('*** execute ***');
-    		console.log('Success:', data);
-    	    	connection.trigger('success');
-  	    })
-  	    .catch(error => {
-    		console.error('Error:', error);
-    	    	connection.trigger('fail');
-  	    });
-        });
     }
 
     function onGetEndpoints(endpoints) {
