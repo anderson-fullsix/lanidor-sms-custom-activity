e2821584a43f538184d6b4b01feeeb30a9ede9ba
anderson-fullsix <144189416+anderson-fullsix@users.noreply.github.com>
2025-06-02 16:16:01 -0300

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -1,13 +1,15 @@
 'use strict';
-var util = require('util');
+
+require('dotenv').config();
 
 const Path = require('path');
 const JWT = require(Path.join(__dirname, '..', 'lib', 'jwtDecoder.js'));
-var util = require('util');
-var http = require('https');
 const axios = require('axios');
 
 exports.logExecuteData = [];
+
+let cachedToken = null;
+let tokenExpiresAt = 0; // em milissegundos
 
 function logData(req) {
     exports.logExecuteData.push({
@@ -31,6 +33,35 @@ function logData(req) {
     });
 }
 
+async function getAuthToken() {
+    const now = Date.now();
+
+    if (cachedToken && tokenExpiresAt > now) {
+        return cachedToken;
+    }
+
+    const authToken = process.env.AUTH_TOKEN;
+    const urlLogin = process.env.URL_LOGIN;
+
+    const config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: urlLogin,
+        headers: {
+            'Authorization': `Basic ${authToken}`
+        },
+        data: ''
+    };
+
+    const response = await axios.request(config);
+    cachedToken = response.data.token;
+
+    // Ajuste aqui se o servidor retornar tempo de expiração (em segundos)
+    tokenExpiresAt = now + (5 * 60 * 1000); // Assume 5 minutos
+
+    return cachedToken;
+}
+
 exports.edit = function (req, res) {
     logData(req);
     res.send(200, 'Edit');
@@ -42,33 +73,15 @@ exports.save = function (req, res) {
 };
 
 exports.execute = function (req, res) {
-require('dotenv').config();
     JWT(req.body, process.env.jwtSecret, (err, decoded) => {
-
         if (err) {
             console.error(err);
             return res.status(401).end();
         }
 
         if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
-            var token = null
-            const authToken = process.env.AUTH_TOKEN;
-            const urlLogin = process.env.URL_LOGIN;
-            
-            const config_get = {
-              method: 'get',
-              maxBodyLength: Infinity,
-              url: urlLogin,
-              headers: {
-                'Authorization': `Basic ${authToken}`,
-              },
-              data: '',
-            };
-
-            axios.request(config_get)
-            .then((response) => {
-                token = response.data.token
-
+            getAuthToken()
+            .then((token) => {
                 let recipients = [];
                 let mobileNumber = '';
                 let variavel = '';
@@ -77,23 +90,11 @@ require('dotenv').config();
                 let messageText = '';
 
                 decoded.inArguments.forEach(arg => {
-                
-                    if (arg.variavel) {
-                        variavel = arg.variavel;
-                    }
-                
-                    if (arg.variavel2) {
-                        variavel2 = arg.variavel2;
-                    }
-                
-                    if (arg.variavel3) {
-                        variavel3 = arg.variavel3;
-                    }
-                    
-                    if (arg.text) {
-                        messageText = arg.text;
-                    }
-    
+                    if (arg.variavel) variavel = arg.variavel;
+                    if (arg.variavel2) variavel2 = arg.variavel2;
+                    if (arg.variavel3) variavel3 = arg.variavel3;
+                    if (arg.text) messageText = arg.text;
+
                     if (arg.recipients) {
                         arg.recipients.forEach(obj => {
                             for (let key in obj) {
@@ -106,60 +107,53 @@ require('dotenv').config();
                     }
                 });
 
-                if (variavel) {
-                    messageText = messageText.replace("<<variavel>>", variavel);
-                }
-                
-                if (variavel2) {
-                    messageText = messageText.replace("<<variavel2>>", variavel2);
-                }
-                
-                if (variavel3) {
-                    messageText = messageText.replace("<<variavel3>>", variavel3);
-                }
-        
+                if (variavel) messageText = messageText.replace("<<variavel>>", variavel);
+                if (variavel2) messageText = messageText.replace("<<variavel2>>", variavel2);
+                if (variavel3) messageText = messageText.replace("<<variavel3>>", variavel3);
+
                 const uniqueMessageId = generateUniqueMessageId(mobileNumber);
-                
-                let data = JSON.stringify({
-                  "id": uniqueMessageId,
-                  "description": decoded.inArguments[0].description,
-                  "sender": decoded.inArguments[0].sender,
-                  "partnerId": decoded.inArguments[0].partnerId,
-                  "text": messageText,
-                  "variavel": decoded.inArguments[0].variavel,
-                  "variavel2": decoded.inArguments[0].variavel2,
-                  "variavel3": decoded.inArguments[0].variavel3,
-                  "sendnow": "true",
-                  "recipients": recipients
+
+                const data = JSON.stringify({
+                    "id": uniqueMessageId,
+                    "description": decoded.inArguments[0].description,
+                    "sender": decoded.inArguments[0].sender,
+                    "partnerId": decoded.inArguments[0].partnerId,
+                    "text": messageText,
+                    "variavel": variavel,
+                    "variavel2": variavel2,
+                    "variavel3": variavel3,
+                    "sendnow": "true",
+                    "recipients": recipients
                 });
-    
+
                 const urlAction = process.env.URL_ACTION;
-                    
-                let config_post = {
-                  method: 'post',
-                  maxBodyLength: Infinity,
-                  url: urlAction,
-                  headers: { 
-                    'Content-Type': 'application/json', 
-                    'Authorization': 'Bearer ' + token
-                  },
-                  data : data
+
+                const config_post = {
+                    method: 'post',
+                    maxBodyLength: Infinity,
+                    url: urlAction,
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer ' + token
+                    },
+                    data: data
                 };
 
                 axios.request(config_post)
                 .then((response) => {
+                    console.log("Mensagem enviada com sucesso:", response.data);
                 })
                 .catch((error) => {
-                    console.log("Error axios.request response: ", error);
+                    console.error("Erro ao enviar mensagem:", error);
                 });
+
+                logData(req);
+                res.send(200, 'Execute');
             })
             .catch((error) => {
-              console.log("Error axios.request: ", error);
+                console.error("Erro ao obter token:", error);
+                return res.status(500).send("Erro na autenticação");
             });
-
-            var decodedArgs = decoded.inArguments[0];
-            logData(req);
-            res.send(200, 'Execute');
         } else {
             console.error('inArguments invalid.');
             return res.status(400).end();
